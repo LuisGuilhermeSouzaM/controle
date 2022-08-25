b740b12a7316f760f014390b28ef00c628ba4341
LuisGuilherme-SM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2022-08-25 16:57:32 -0300

    finalizando trabalho

--- ErroQuadratico.m
+++ ErroQuadratico.m
@@ -1,4 +1,4 @@
-function [EQ] = ErroQuadraticoSIR(X,Rd,Id, Sd)
+function [Eq] = ErroQuadraticoSIR(X,Rd,Id)
 % Esta função determina o erro quadrático
 % entre a saída real e a saída do modelo.
 % u - entrada real
@@ -6,37 +6,32 @@ function [EQ] = ErroQuadraticoSIR(X,Rd,Id, Sd)
 % t - tempo real
 % X = [Km pm];
 
-%% Tamanho da população - N
-
 beta = X(1);
 r = X(2);
 
-%%% Modelo
-mdl = 'SimulacaoSIR';
-load_system(mdl)
-cs = getActiveConfigSet(mdl);
-mdl_cs = cs.copy;
-set_param(mdl_cs,'AbsTol','1e-5',...
-         'SaveState','on','StateSaveName','xoutNew',...
-         'SaveOutput','on','OutputSaveName','youtNew');
-simOut = sim(mdl, mdl_cs);
+
+%%% Tamanho da população - N
+N = 500;
+
+%%% Valores iniciais do modelo
+i0 = 1;
+r0 = 0;
+s0 = N - i0;
+
+options = simset('solver','ode45','MaxStep',0.01,'InitialStep',0.01,'ReturnWorkspaceOutputs', 'on');
+y = sim("SimulacaoSIR.slx",1000,options);
 
 %%% Saida do modelo
-S = simOut.logsout{1}.Values.Data; % Saída I
-I = simOut.logsout{2}.Values.Data; % Saída I
-R = simOut.logsout{3}.Values.Data; % Saída R
 
-S = resample(S,length(S),length(Sd));
+I = y.logsout{1}.Values.Data; % Saída I
+R = y.logsout{2}.Values.Data; % Saída R
+
 I = resample(I,length(I),length(Id));
 R = resample(R,length(R),length(Rd));
 
 %%% Erro quadrático
 
-% Eq = sum((I - Id).^2) + sum((R - Rd).^2); %% cálculo do erro
-erroS = S - Sd;
-erroI = I - Id;
-erroR = R - Rd;
-EQ = horzcat(erroS,erroI,erroR);
+Eq = sum((I - Id).^2) + sum((R - Rd).^2); %% cálculo do erro
 
 % plot(t,y,t,ym,"linewidth", 2)
 
